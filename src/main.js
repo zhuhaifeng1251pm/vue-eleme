b3b2aeb1dd8656d991a3436c2bdf06b4bcbee493
zhuhaifeng1251pm <18833527317@139.com>
2018-09-17 10:00:33 +0800

    date change Chinese

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ Vue.config.productionTip = false;
 import router from "./router/";
 import store from './store/index'
 import moment from 'moment'//导入文件
+import '../node_modules/moment/locale/zh-cn'
 
 Vue.prototype.$moment = moment;//赋值使用
 new Vue({
